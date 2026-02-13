24d2e63d2597d70c19c799ece263d9a1b5003df9
labestiagpt <labestiagpt@gmail.com>
2026-02-13 16:34:53 +0100

    Add ResetButtonHandler for default physics reset functionality

---
+++ src/main/java/com/labestiagpt/mwater/menu/ResetButtonHandler.java
@@ -0,0 +1,11 @@
+// ResetButtonHandler.java
+
+package com.labestiagpt.mwater.menu;
+
+public class ResetButtonHandler {
+
+    public void resetToDefaultPhysics() {
+        // Implementation for resetting physics settings to default values
+        System.out.println("Physics settings reset to default.");
+    }
+}
