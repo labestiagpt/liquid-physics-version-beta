a4d41efc7957195e70e099adf707d0e63ef53bb1
labestiagpt <labestiagpt@gmail.com>
2026-02-13 16:34:45 +0100

    Create main custom menu screen with title mWater V1.0.0 Beta

---
+++ src/main/java/com/labestiagpt/mwater/menu/MWaterMainMenu.java
@@ -0,0 +1,8 @@
+package com.labestiagpt.mwater.menu;
+
+public class MWaterMainMenu {
+    public static void main(String[] args) {
+        System.out.println("Welcome to mWater V1.0.0 Beta");
+        // Additional menu functionality can be added here
+    }
+}
