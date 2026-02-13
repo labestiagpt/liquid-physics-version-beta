606d54194a935deaf4d6f04d33202255be907f7c
labestiagpt <labestiagpt@gmail.com>
2026-02-13 16:34:15 +0100

    Create CubeFluidHandler.java for cube-fluid interaction handling

---
+++ src/main/java/com/labestiagpt/mwater/interaction/CubeFluidHandler.java
@@ -0,0 +1,15 @@
+// CubeFluidHandler.java
+
+package com.labestiagpt.mwater.interaction;
+
+public class CubeFluidHandler {
+    // Class for handling interactions between cube and fluid
+
+    public CubeFluidHandler() {
+        // Constructor logic
+    }
+
+    public void handleInteraction() {
+        // Method for handling interaction logic
+    }
+}
